8906e03da83160a1f6d396dc448a8f42044c9421
kh14762 <kevin.j.heritage@gmail.com>
2020-10-29 11:12:51 -0400

    added thread ids and pthread functions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <fstream.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <vector>
 
 using namespace std;
 
+void *sifter (char *);
+void *decode (char *);
+void *substitute (char *);
+void *hill (char *);
+void *peak (char *);
+
 int main()
 {
+    pthread_t sifter_id;
+    pthread_t decoder_id;
+    pthread_t substitute_id;
+    pthread_t hill_id;
+    pthread_t peak_id;
+
     cout << "Hello World! its me Kevin!";
     return 0;
 }
