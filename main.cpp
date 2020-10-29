b81df01dd5f04dacff9080716913dd9708e8ba48
kh14762 <kevin.j.heritage@gmail.com>
2020-10-29 12:02:56 -0400

    created sifter thread and printed out message

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <fstream.h>
+#include <fstream>
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
@@ -8,11 +8,11 @@
 
 using namespace std;
 
-void *sifter (char *);
-void *decode (char *);
-void *substitute (char *);
-void *hill (char *);
-void *peak (char *);
+void *sifter (void *);
+void *decode (void *);
+void *substitute (void *);
+void *hill (void *);
+void *peak (void *);
 
 int main()
 {
@@ -21,8 +21,27 @@ int main()
     pthread_t substitute_id;
     pthread_t hill_id;
     pthread_t peak_id;
+    void* sifterPointer;
+    int result;
+    char *message;
 
-    cout << "Hello World! its me Kevin!";
-    return 0;
+    result = pthread_create(&sifter_id, NULL, sifter, (void*)message);
+    if (result != 0)
+    {
+        perror("Thread Creation FAILED");
+        exit(EXIT_FAILURE);
+    }
+    result = pthread_join(sifter_id, &sifterPointer);
+    if (result != 0)
+    {
+        perror("Join FAILED");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void *sifter(void *)
+{
+    cout << "Hello this is your sifter thread speaking" << endl;
+    pthread_exit(0);
 }
 
